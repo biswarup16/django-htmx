42ed293978deb2083c7ffb6d51fa547cf2ecf6cc
biswarup16 <biswarupdaslyf@gmail.com>
2022-10-25 10:05:17 -0700

    updated

--- project/htmx/models.py
+++ project/htmx/models.py
@@ -1,3 +1,12 @@
+import math
 from django.db import models
+import random
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    roll_no = models.CharField(max_length=20)
+
+    def __str__(self):
+        return str(self.name)
--- project/htmx/views.py
+++ project/htmx/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
+from . models import *
 
 # Create your views here.
 
@@ -12,14 +13,24 @@ def search(request):
     print(username)
     mylist = ["apple", "banana", "cherry"]
 
-    # for i in mylist:
-    if username in mylist:
-            # return HttpResponse('<div style="color:red">The Username is Already Exists</div>')
-            context={'obj':username}
-            # print(i + 'hello')
-            return render(request,'search.html',context)
+    if len(username) >0:
+
+
+        student_obj = Student.objects.filter(name__icontains=username)[:3]
+
+        return render(request,'search.html',{'student_obj':student_obj})
     else:
-            return HttpResponse('<div style="color:red">The Fruit in Not Available</div>')       
+        return render(request,'search.html',{'student_obj':None})
+
+
+    # # for i in mylist:
+    # if username in mylist:
+    #         # return HttpResponse('<div style="color:red">The Username is Already Exists</div>')
+    #         context={'obj':username}
+    #         # print(i + 'hello')
+    #         return render(request,'search.html',context)
+    # else:
+    #         return HttpResponse('<div style="color:red">The Fruit in Not Available</div>')       
 
 
     
